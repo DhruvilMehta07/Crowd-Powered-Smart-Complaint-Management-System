4d65a9652734aee71699882bd91ae2524bd54ced
agrawalsam55 <135312926+agrawalsam55@users.noreply.github.com>
2025-09-23 12:23:25 +0530

    Create Help.jsx

---
+++ Frontend/src_home/pages/Help.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Help() {
+  return (
+    <div className="flex-1 p-6">
+      <h1 className="text-xl font-bold">Help & Support</h1>
+      <p className="mt-4 text-gray-600">
+        For any queries, please contact support@complaints.com
+      </p>
+    </div>
+  );
+}
